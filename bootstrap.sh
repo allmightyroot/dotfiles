b69795532322111de35e9713ace9dd00ebce1397
Chris Beason <allmightyroot@gmail.com>
2023-02-02 16:00:18 -0600

    Initial Import

---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This file is used to bootstrap a github codespaces workspace with my usual standards, etc
+# This sort of combines my usual ansible based sysprep stuff with one script
+# Sources of inspiration: 
+# https://www.huuhka.net/personalizing-your-github-codespaces/
+# https://bea.stollnitz.com/blog/codespaces-terminal/
+
+
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
+export INSTALL_ZSH=true
+export USERNAME=`whoami`
+
+## update and install my usual packages (minus some that aren't needed in codespaces)
+sudo apt-get update
+sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
+sudo apt-get install -y \
+  curl \
+  dstat \
+  git \
+  glances \
+  gnupg2 \
+  htop \
+  jq \
+  less \
+  lsb-release \
+  multitail \
+  ncdu \
+  openssh-client \
+  p7zip \
+  powerline \
+  python3-pip \
+  python3-virtualenv \
+  sudo \
+  wget \
+  unzip \
+  vim-nox
+
+# Install & Configure Zsh
+if [ "$INSTALL_ZSH" = "true" ]
+then
+
+
+    cp -f ~/dotfiles/zshrc/.zshrc ~/.zshrc
+    cp -f ~/dotfiles/zshrc/.zshrc-antigen-common ~/.zshrc-common
+    mkdir -p ~/.repos/externalgit
+    pushd `pwd`
+    cd ~/.repos/externalgit
+    git clone https://github.com/zsh-users/antigen.git
+    popd
+    chsh -s /usr/bin/zsh $USERNAME
+fi
+
+# Cleanup
+sudo apt-get autoremove -y
+sudo apt-get autoremove -y
+sudo rm -rf /var/lib/apt/lists/*
---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+/opt/homebrew/bin/oh-my-posh init pwsh --config '/opt/homebrew/opt/oh-my-posh/themes/mojada.omp.json' | Invoke-Expression
+
+Set-PSReadLineOption -predictionsource history
